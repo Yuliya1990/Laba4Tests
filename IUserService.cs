4094de8f684412fcadec15a203535d2a9d49fa2d
Yuliya1990 <84015911+Yuliya1990@users.noreply.github.com>
2023-05-11 11:19:57 +0200

    Add files via upload

---
+++ IUserService.cs
@@ -0,0 +1,10 @@
+﻿namespace EasylifeAPI.Services
+{
+    public interface IUserService
+    {
+        List<User> GetUsers();
+        User GetById(int id);
+        void Register(UserRegister newUser);
+        
+    }
+}
